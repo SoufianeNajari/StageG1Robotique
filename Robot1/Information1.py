59e3cd0124b04978162827c9d1de349c26e87bb7
Fayaz <nawid.fayaz@gmail.com>
2023-02-01 14:50:32 +0100

    Dépôt prog robot

---
+++ Robot1/Information1.py
@@ -0,0 +1,91 @@
+import queue
+import socket
+import threading
+from megapi import *
+import random
+
+
+host = "172.31.208.173"
+port = 4455
+addr = (host, port)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+u =0
+
+
+def onReadDist(v):
+    msg = "Distance " + str(v)
+    msg = msg.encode("utf-8")
+    client.sendto(msg, addr)
+
+
+def onReadVit(v):
+    msg = "Vitesse " + str(v)
+    msg = msg.encode("utf-8")
+    client.sendto(msg, addr)
+    
+
+
+def getVal(v):
+    global u
+    u = v
+    
+    
+    
+if __name__ == "__main__":
+    name = "Robot1"
+    """ Creating the UDP socket """
+    data = "Connexion"
+    bot = MegaPi()
+    bot.start()
+    vitesseD = 30
+    vitesseG = 30
+    bot.encoderMotorRun(1,vitesseD);
+    bot.encoderMotorRun(2, -vitesseG);
+    sleep(1);
+    
+    
+    
+    
+    
+    def receive():
+        global vitesseD
+        global vitesseG
+        while True:
+            try:
+                message, addr = client.recvfrom(1024)
+                info = message.decode("utf-8").split()
+                if info[0] == 'Distance':
+                    if float(info[1]) < 10:
+                        vitesseD = vitesseD + 15
+                        vitesseG = vitesseD
+                print(message)
+            except:
+                pass
+          
+                           
+    def send():
+        while True:  
+            sleep(1);
+            #bot.ultrasonicSensorRead(8,onReadDist);
+            bot.encoderMotorSpeed(1,onReadVit);
+            
+            
+    t1 = threading.Thread(target=receive)
+    t2 = threading.Thread(target=send)
+
+    t1.start()
+    t2.start()
+    
+    
+    while True:
+        sleep(0.03)
+        bot.ultrasonicSensorRead(8, getVal)
+        if u <20:
+            bot.encoderMotorRun(1, 0)
+            vitesseG = -vitesseD - vitesseD//2 
+        else:
+            
+            vitesseG = vitesseD
+        bot.encoderMotorRun(1, vitesseD)
+        bot.encoderMotorRun(2, -vitesseG)
---
+++ Robot2/server.py
@@ -0,0 +1,71 @@
+import queue
+import socket
+import threading
+from megapi import *
+from math import *
+
+
+if __name__ == "__main__":
+    host = "172.31.208.173"
+    port = 4455
+    """ Creating the UDP socket """
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    """ Bind the host address with the port """
+    server.bind((host, port))
+    l = [0, 0, 0]
+    ip1 = '10.3.141.101'
+    ip3 = '172.31.208.174'
+
+    
+    messages = queue.Queue()
+    bot = MegaPi()
+    bot.start()
+    vitesse = 30
+    bot.encoderMotorRun(1, vitesse);
+    bot.encoderMotorRun(2, -vitesse);
+    sleep(1);
+    
+    
+    
+    def receive():
+        while True:
+            try:
+                message, addr = server.recvfrom(1024)
+                messages.put((message, addr))
+            except:
+                pass
+          
+                           
+    def send():
+        global vitesse
+        while True:
+            while not messages.empty():
+                message, addr = messages.get()
+                if not addr in l and addr[0] == ip1:
+                    l[0] = addr
+                if not addr in l and addr[0] == ip3:
+                    l[2] = addr
+                print(message.decode("utf-8"))  
+                info = message.decode("utf-8").split()
+                if info[0] ==  'Distance':
+                    if addr == l[2] and l[0]!=0:
+                        server.sendto(message, l[0])
+                if info[0] == 'Vitesse':
+                    if addr == l[0]:
+                        vitesse = floor(float(info[1]))
+                               
+                  
+    t1 = threading.Thread(target=receive)
+    t2 = threading.Thread(target=send)
+
+    t1.start()
+    t2.start()
+    
+    
+    while True:
+        sleep(0.5)
+        bot.encoderMotorRun(1, vitesse)
+        bot.encoderMotorRun(2, -vitesse)
+    
+    
+    
